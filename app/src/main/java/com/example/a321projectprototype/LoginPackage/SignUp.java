398aa6acc072c0cc3d0963b56f3383e300070a64
brendansUNIwork <bsm602@uowmail.edu.au>
2022-03-08 21:36:24 +1100

    Updated Sign In process
    update process is now more clean when registering

--- app/src/main/java/com/example/a321projectprototype/LoginPackage/SignUp.java
+++ app/src/main/java/com/example/a321projectprototype/LoginPackage/SignUp.java
@@ -27,6 +27,7 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
@@ -42,7 +43,7 @@ public class SignUp extends AppCompatActivity {
     private int id;
     private EditText fullNameEditText, usernameEditText, emailEditText, password1EditText, password2EditText;
     private Button register;
-    private TextView returnText;
+    private TextView returnText, registerText, registerAccountAlready;
     private UserModel user;
     private ConstraintLayout registerLayout;
     private UserDatabase userDatabase;
@@ -54,6 +55,7 @@ public class SignUp extends AppCompatActivity {
     private FirebaseAuth auth;
     private String userFirebaseId;
     private FirebaseFirestore firebaseFirestore;
+    private FirebaseUser firebaseUser;
     private final String USERS_KEY = "qbPjDm73CVIUz63gDu8D";
 
 
@@ -66,8 +68,7 @@ public class SignUp extends AppCompatActivity {
 
         System.out.println(amountOfUsers);
 
-        auth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
+
 
 
 
@@ -79,8 +80,11 @@ public class SignUp extends AppCompatActivity {
         register = findViewById(R.id.registerButton);
         registerLayout = findViewById(R.id.registerlayer);
         returnText = findViewById(R.id.registerSignIn);
+        registerText = findViewById(R.id.registerText);
+        registerAccountAlready = findViewById(R.id.registerAccountAlready);
         progressBar = findViewById(R.id.signUpProgressBar);
         progressBar.setVisibility(View.INVISIBLE);
+
 
         returnText.setOnClickListener(returnMethod);
         register.setOnClickListener(registerMethod);
@@ -91,6 +95,8 @@ public class SignUp extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
+            progressBar.setVisibility(View.VISIBLE);
+            setInvisiable();
             fullNameInput = fullNameEditText.getText().toString();
             usernameInput = usernameEditText.getText().toString();
             emailInput = emailEditText.getText().toString();
@@ -101,58 +107,84 @@ public class SignUp extends AppCompatActivity {
             if (fullNameInput.isEmpty()) {
                 fullNameEditText.setError("First name is requried");
                 fullNameEditText.requestFocus();
+                setVisiable();
+                setProgress();
             }
 
             if (usernameInput.isEmpty()) {
                 usernameEditText.setError("Last name is requried");
                 usernameEditText.requestFocus();
+                setVisiable();
+                setProgress();
             }
 
             if (emailInput.isEmpty()) {
                 emailEditText.setError("Email is requried");
                 emailEditText.requestFocus();
+                setVisiable();
+                setProgress();
             }
 
             if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
                 emailEditText.setError("Email is required");
                 emailEditText.requestFocus();
+                setVisiable();
+                setProgress();
             }
 
             if (password1Input.isEmpty()) {
                 password1EditText.setError("Password is required");
                 password1EditText.requestFocus();
+                setVisiable();
+                setProgress();
             }
             if (password2Input.isEmpty()) {
                 password2EditText.setError("Password is required");
                 password2EditText.requestFocus();
+                setVisiable();
+                setProgress();
 
             }
 
             if (password1Input.matches(password2Input) && !password1Input.isEmpty())
             {
 
+                auth = FirebaseAuth.getInstance();
+                firebaseFirestore = FirebaseFirestore.getInstance();
+
                 auth.createUserWithEmailAndPassword(emailInput,password2Input).addOnCompleteListener((task -> {
 
 
 
-                    userFirebaseId = auth.getCurrentUser().getUid();
-                    DocumentReference documentReference = firebaseFirestore.collection("users").document(userFirebaseId);
-                    HashMap<String,Object> userMap = new HashMap<>();
-                    userMap.put("fullname",fullNameInput);
-                    userMap.put("username",usernameInput);
-                    userMap.put("password",password1Input);
-                    userMap.put("email", emailInput);
-                    documentReference.set(userMap).addOnSuccessListener(new OnSuccessListener<Void>()
+                    if(task.isSuccessful())
                     {
-                        @Override
-                        public void onSuccess(Void aVoid)
-                        {
-                            System.out.println("worked");
-                        }
-                    });
-                    Snackbar.make(registerLayout, "Contact Has Been Saved ", Snackbar.LENGTH_LONG).show();
-                    returnToLogin();
 
+                        userFirebaseId = auth.getCurrentUser().getUid();
+                        DocumentReference documentReference = firebaseFirestore.collection("users").document(userFirebaseId);
+                        HashMap<String,Object> userMap = new HashMap<>();
+                        userMap.put("fullname",fullNameInput);
+                        userMap.put("username",usernameInput);
+                        userMap.put("password",password1Input);
+                        userMap.put("email", emailInput);
+                        documentReference.set(userMap).addOnSuccessListener(new OnSuccessListener<Void>()
+                        {
+                            @Override
+                            public void onSuccess(Void aVoid)
+                            {
+                                System.out.println("worked");
+                            }
+                        });
+
+                        returnToLogin();
+                    }
+                    else
+                    {
+                        setVisiable();
+                        setProgress();
+                        emailEditText.setError("Your Email Is Already In Use");
+                        emailEditText.requestFocus();
+
+                    }
                 }));
             }
 
@@ -174,6 +206,37 @@ public class SignUp extends AppCompatActivity {
     private void returnToLogin()
     {
         finish();
+    }
+
+    private void setProgress()
+    {
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void setInvisiable()
+    {
+        fullNameEditText.setVisibility(View.INVISIBLE);
+        usernameEditText.setVisibility(View.INVISIBLE);
+        emailEditText.setVisibility(View.INVISIBLE);
+        password1EditText.setVisibility(View.INVISIBLE);
+        password2EditText.setVisibility(View.INVISIBLE);
+        register.setVisibility(View.INVISIBLE);
+        returnText.setVisibility(View.INVISIBLE);
+        registerAccountAlready.setVisibility(View.INVISIBLE);
+        registerText.setVisibility(View.INVISIBLE);
+    }
+
+    private void setVisiable()
+    {
+        fullNameEditText.setVisibility(View.VISIBLE);
+        usernameEditText.setVisibility(View.VISIBLE);
+        emailEditText.setVisibility(View.VISIBLE);
+        password1EditText.setVisibility(View.VISIBLE);
+        password2EditText.setVisibility(View.VISIBLE);
+        register.setVisibility(View.VISIBLE);
+        returnText.setVisibility(View.VISIBLE);
+        registerAccountAlready.setVisibility(View.VISIBLE);
+        registerText.setVisibility(View.VISIBLE);
     }
 
 }
