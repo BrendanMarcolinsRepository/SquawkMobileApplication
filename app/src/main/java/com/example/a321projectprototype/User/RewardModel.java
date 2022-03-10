c34a45fed13c19c72e187d5898774cfe37b77919
brendansUNIwork <bsm602@uowmail.edu.au>
2022-03-10 14:17:15 +1100

    update record and discover sections

---
+++ app/src/main/java/com/example/a321projectprototype/User/RewardModel.java
@@ -0,0 +1,17 @@
+package com.example.a321projectprototype.User;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.DocumentReference;
+
+public class RewardModel {
+    private final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference ref = db.getReference("https://sqawkandroidapplication.firebaseio.com");
+
+    ref.
+
+    public int getMonthlyScore() {
+
+    }
+}
