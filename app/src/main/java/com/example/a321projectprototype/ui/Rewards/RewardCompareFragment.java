2c677e085cd6155af65c622b6fe24fd9b1caf356
Linus Knowlson <71065813+LinusKnowlson@users.noreply.github.com>
2022-03-11 15:28:56 +1100

    Update RewardCompareFragment.java

--- app/src/main/java/com/example/a321projectprototype/ui/Rewards/RewardCompareFragment.java
+++ app/src/main/java/com/example/a321projectprototype/ui/Rewards/RewardCompareFragment.java
@@ -7,38 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import androidx.cardview.widget.CardView;
 
 import com.example.a321projectprototype.R;
 
+import java.util.ArrayList;
+
 public class RewardCompareFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Spinner rewardSpinner;
+    private ArrayList<String> times;
+    private ArrayAdapter<String> adaptor;
+    private CardView cardView;
+    private View root;
 
     public RewardCompareFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RewardCompareFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RewardCompareFragment newInstance(String param1, String param2) {
         RewardCompareFragment fragment = new RewardCompareFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -46,16 +37,26 @@ public class RewardCompareFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reward_compare, container, false);
+        root = inflater.inflate(R.layout.fragment_reward_compare, container, false);
+        rewardSpinner = root.findViewById(R.id.reward_compare_spinner);
+        times = new ArrayList<>();
+        times.add("Today");
+        times.add("This Week");
+        times.add("This Month");
+        times.add("Last Week");
+        times.add("This Year");
+        adaptor = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_dropdown_item, times);
+        rewardSpinner.setAdapter(adaptor);
+        return root;
+    }
+
+    public void showCompareBar(View view){
+        cardView = root.findViewById(R.id.reward_compare_userTxt);
+        cardView.setVisibility(View.VISIBLE);
     }
 }
