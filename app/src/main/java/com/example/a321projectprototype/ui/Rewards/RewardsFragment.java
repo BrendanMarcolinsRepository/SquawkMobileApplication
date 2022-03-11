c4f1fee149d7226dcd41cdea0c9b747910ebde58
Linus Knowlson <71065813+LinusKnowlson@users.noreply.github.com>
2022-03-11 15:25:54 +1100

    Update RewardsFragment.java

--- app/src/main/java/com/example/a321projectprototype/ui/Rewards/RewardsFragment.java
+++ app/src/main/java/com/example/a321projectprototype/ui/Rewards/RewardsFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +21,8 @@ import com.example.a321projectprototype.HomePage;
 import com.example.a321projectprototype.R;
 import com.example.a321projectprototype.ui.Flock.FlockViewModel;
 
+import java.util.ArrayList;
+
 public class RewardsFragment extends Fragment
 {
     private RewardsViewModel rewardsViewModel;
@@ -26,6 +30,9 @@ public class RewardsFragment extends Fragment
     private CardView achievementButton;
     private NavController navController;
     private HomePage homePage;
+    private Spinner rewardSpinner;
+    private ArrayList<String> times;
+    private ArrayAdapter<String> adaptor;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rewardsViewModel = new ViewModelProvider(this).get(RewardsViewModel.class);
@@ -38,6 +45,16 @@ public class RewardsFragment extends Fragment
 
         compareButton.setOnClickListener(rewardCompare);
         achievementButton.setOnClickListener(rewardAchievement);
+
+        rewardSpinner = root.findViewById(R.id.reward_compare_spinner);
+        times = new ArrayList<>();
+        times.add("Today");
+        times.add("This Week");
+        times.add("This Month");
+        times.add("Last Week");
+        times.add("This Year");
+        adaptor = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_dropdown_item, times);
+        rewardSpinner.setAdapter(adaptor);
 
         return root;
     }
