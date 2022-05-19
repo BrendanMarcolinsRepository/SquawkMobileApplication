4f4034a15fe15033c4f6b38ecd058b79eb82ff29
Linus Knowlson <71065813+LinusKnowlson@users.noreply.github.com>
2022-05-20 00:48:54 +1000

    Update SettingsPermissionFragment.java

--- app/src/main/java/com/example/a321projectprototype/ui/Settings/SettingsPermissionFragment.java
+++ app/src/main/java/com/example/a321projectprototype/ui/Settings/SettingsPermissionFragment.java
@@ -7,38 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.a321projectprototype.R;
 
 public class SettingsPermissionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageView mediaImageView, fileImageView, storageImageView, audioImageView;
+    private int mediaClick, fileClick, storageClick, audioClick = 1;
 
     public SettingsPermissionFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsPermissionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsPermissionFragment newInstance(String param1, String param2) {
+    public static SettingsPermissionFragment newInstance() {
         SettingsPermissionFragment fragment = new SettingsPermissionFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -46,16 +30,71 @@ public class SettingsPermissionFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_permission, container, false);
+        View root = inflater.inflate(R.layout.fragment_settings_permission, container, false);
+        mediaImageView = root.findViewById(R.id.permissions_slideButtonDisable1);
+        fileImageView = root.findViewById(R.id.permissions_slideButtonEnable2);
+        storageImageView = root.findViewById(R.id.permissions_slideButtonEnable3);
+        audioImageView = root.findViewById(R.id.permissions_slideButtonDisable4);
+        //set Listeners
+        mediaImageView.setOnClickListener(mediaImageViewHandler);
+        fileImageView.setOnClickListener(fileImageViewHandler);
+        storageImageView.setOnClickListener(storageImageHandler);
+        audioImageView.setOnClickListener(audioViewHandler);
+
+        return root;
     }
+    // handle slide bar click events
+    private final View.OnClickListener mediaImageViewHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mediaClick%2 == 1){
+                mediaImageView.setImageResource(R.drawable.slidebutton2);
+            }else{
+                mediaImageView.setImageResource(R.drawable.slidebutton1);
+            }
+            mediaClick++;
+        }
+    };
+
+    private final View.OnClickListener fileImageViewHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(fileClick%2 == 1){
+                fileImageView.setImageResource(R.drawable.slidebutton1);
+            }else{
+                fileImageView.setImageResource(R.drawable.slidebutton2);
+            }
+            fileClick++;
+        }
+    };
+
+    private final View.OnClickListener storageImageHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(storageClick%2 == 1){
+                storageImageView.setImageResource(R.drawable.slidebutton1);
+            }else{
+                storageImageView.setImageResource(R.drawable.slidebutton2);
+            }
+            storageClick++;
+        }
+    };
+
+    private final View.OnClickListener audioViewHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(audioClick%2 == 1){
+                audioImageView.setImageResource(R.drawable.slidebutton2);
+            }else{
+                audioImageView.setImageResource(R.drawable.slidebutton1);
+            }
+            audioClick++;
+        }
+    };
 }
