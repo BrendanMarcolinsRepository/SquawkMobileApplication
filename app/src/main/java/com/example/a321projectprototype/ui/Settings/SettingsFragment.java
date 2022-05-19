8017ac05462881d2ada169fe617a7b42aab9211c
Linus Knowlson <71065813+LinusKnowlson@users.noreply.github.com>
2022-05-20 00:48:06 +1000

    Update SettingsFragment.java

--- app/src/main/java/com/example/a321projectprototype/ui/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/a321projectprototype/ui/Settings/SettingsFragment.java
@@ -5,15 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.example.a321projectprototype.HomePage;
 import com.example.a321projectprototype.R;
@@ -26,6 +22,8 @@ public class SettingsFragment extends Fragment
     private ImageView termsButton;
     private NavController navController;
     private HomePage homePage;
+    private ImageView backupImageView, autoImageView, securityImageView, usageImageView;
+    private int backupClickNum, autoClickNum, securityClickNum, imageClickNum = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         settingViewModel = new ViewModelProvider(this).get(SettingViewModel.class);
@@ -39,9 +37,20 @@ public class SettingsFragment extends Fragment
         permissionButton.setOnClickListener(settingsPermissions);
         termsButton.setOnClickListener(settingsTerms);
 
+        backupImageView = root.findViewById(R.id.settings_slideButtonDisable1);
+        autoImageView = root.findViewById(R.id.settings_slideButtonEnable2);
+        securityImageView = root.findViewById(R.id.settings_slideButtonEnable3);
+        usageImageView = root.findViewById(R.id.settings_slideButtonDisable5);
+
+        //set Listeners
+        backupImageView.setOnClickListener(backupImageViewHandler);
+        autoImageView.setOnClickListener(autoImageViewHandler);
+        securityImageView.setOnClickListener(securityImageHandler);
+        usageImageView.setOnClickListener(imageViewHandler);
+
         return root;
     }
-    // Navigations
+    // Navigation
     private final View.OnClickListener settingsPermissions = new View.OnClickListener()
     {
         @Override
@@ -57,6 +66,54 @@ public class SettingsFragment extends Fragment
         public void onClick(View v)
         {
             navController.navigate(R.id.action_nav_Settings_to_settingsTermFragment);
+        }
+    };
+    // handle slide bar click events
+    private final View.OnClickListener backupImageViewHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(backupClickNum%2 == 1){
+                backupImageView.setImageResource(R.drawable.slidebutton2);
+            }else{
+                backupImageView.setImageResource(R.drawable.slidebutton1);
+            }
+            backupClickNum++;
+        }
+    };
+
+    private final View.OnClickListener autoImageViewHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(autoClickNum%2 == 1){
+                autoImageView.setImageResource(R.drawable.slidebutton1);
+            }else{
+                autoImageView.setImageResource(R.drawable.slidebutton2);
+            }
+            autoClickNum++;
+        }
+    };
+
+    private final View.OnClickListener securityImageHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(securityClickNum%2 == 1){
+                securityImageView.setImageResource(R.drawable.slidebutton1);
+            }else{
+                securityImageView.setImageResource(R.drawable.slidebutton2);
+            }
+            securityClickNum++;
+        }
+    };
+
+    private final View.OnClickListener imageViewHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(imageClickNum%2 == 1){
+                usageImageView.setImageResource(R.drawable.slidebutton2);
+            }else{
+                usageImageView.setImageResource(R.drawable.slidebutton1);
+            }
+            imageClickNum++;
         }
     };
 }
